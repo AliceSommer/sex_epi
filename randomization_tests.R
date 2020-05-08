db67416103d82942402542b1acad9b728719c867
Alice Sommer <alice.j.sommer@gmail.com>
2020-05-08 18:00:30 +0200

    plot histograms of randomization tests

---
+++ randomization_tests.R
@@ -0,0 +1,39 @@
+
+head(cgs)
+head(gap_value)
+head(cgs_interest_melt)
+
+# set the number of randomizations
+nrep <- 10^4
+
+# create a matrix where the t_rand will be saved
+t_arrays <- matrix(NA, ncol=length(cgs), nrow=nrep)
+
+for(i in 1:length(cgs)){
+  print(i)
+  dat_cg = cgs_interest_melt[cgs_interest_melt$cg_name == cgs[i],]
+  for(j in 1:nrep){
+    W_rep = sample(dat_cg$sex)
+    # retrieve the values of the imputed POs for the units assigned to treatment
+    newy_F = dat_cg$value[W_rep == "F"]
+    # retrieve the values of the imputed POs for the units assigned to control
+    newy_M = dat_cg$value[W_rep == "M"]
+    # calculate the test statistic
+    new_gap_stat = stat(newy_F,newy_M)
+    # fill t_arrays 
+    t_arrays[j,i] = new_gap_stat 
+  }
+}
+
+## calculate p_value
+p_values <- apply(t_arrays, 2, function(x) mean(x >= 80))
+
+## melt t_arrays
+t_arrays_data_frame <- data.frame(t_arrays)
+colnames(t_arrays_data_frame) <- cgs
+
+t_array_melt <- melt(t_arrays_data_frame)
+
+ggplot(t_array_melt,aes(x = value)) + 
+  facet_wrap(~variable,scales = "free_x") + 
+  geom_histogram()
