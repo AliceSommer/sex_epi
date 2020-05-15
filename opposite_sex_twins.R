d3f77b4d42313522cef0175ff391509c9bd25c12
Alice Sommer <alice.j.sommer@gmail.com>
2020-05-15 15:02:29 +0200

    change plot settings

--- opposite_sex_twins.R
+++ opposite_sex_twins.R
@@ -12,12 +12,12 @@ opposite_sex_melt$pair_nb[opposite_sex_melt$variable %in% DZ_ID_11] <- "Pair 2" 
 
 g_twin <- ggplot(opposite_sex_melt) +
   geom_point(aes(x = value, y = cg_name, colour = sex)) +
-  facet_grid(scenario ~ pair_nb, scales = "free_y") + theme_minimal() +
+  facet_grid(scenario ~ pair_nb, scales = "free", space = "free") + theme_minimal() +
   ggtitle("Opposite sex twins") + xlab('x 100 (%)')
   
 ggsave(file = 'twin_opposite_sex.jpeg', g_twin,
          dpi=300,
-         width = 180,
-         height = 260,
+         width = 150,
+         height = 160,
          units = "mm")
 
